a47618f11779928d1bdcdfe51513a794afc37fae
Tim Seeger <tim.seeger@stackfuel.com>
2020-02-03 18:02:58 +0100

    angepasst

--- front-page.php
+++ front-page.php
@@ -17,11 +17,12 @@
         </div>
     </header>
 
-    <p class="description">A marketing and brand<br>
-    creation agency.</p>
-    <p class="description_one"> We don't just make logos, we make design & content
-    <br>
-    choices that disrupt the market from the inside out.
+    <p class="description">
+        A marketing and brand<br>
+        creation agency.</p>
+    <p class="description_one"> 
+        We don't just make logos, we make design & content<br>
+        choices that disrupt the market from the inside out.
     <br>
     <br>
     <button class="btn button-started"> 
@@ -30,4 +31,12 @@
     <button class="btn button-more">
         Learn more
     </button>
+</section>
+<section>
+    <?php get_sidebar('links1')?>
+    <?php get_sidebar('rechts1')?>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
+
+Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   
+
 </section>
