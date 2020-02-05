230992df42b4cb9c6868115f95c85fa07d7f6da0
Tim Seeger <tim.seeger@stackfuel.com>
2020-02-05 17:48:59 +0100

    weitere sections hinzugefügt

--- front-page.php
+++ front-page.php
@@ -1,147 +1,281 @@
 <?php get_header(); ?>
 
-<section class="erste_section">       
-<div class="container">
-    <div class="row">
-        <div class="col kleiner">
-            1
-        </div>
-        <div class="col-10">
-            <header>
-                <div class="kopf">
-                    <img src="<?php bloginfo('template_url'); ?>/img/logo_white.svg" class="logo">
-                    <a class="zwischen" href="#"> Home </a>
-                    <a class="zwischen" href="#"> Blog </a>
-                    <a class="zwischen ende" href="#"> Info </a>
-                    <section class="rechts">    
-                        P:(800)997-9714
-                        <a href="#">
-                            <button class="btn button-start">
-                                START A PROJECT
-                            </button>
-                        </a>
-                    </section>
-                </div>
-            </header>
-                <p class="description">
-                    A marketing and brand<br>
-                    creation agency.</p>
-                <p class="description_one"> 
-                    We don't just make logos, we make design & content</p>
-                <p>choices that disrupt the market from the inside out.</p>
-                <button class="btn button-started"> 
-                    Get started
-                </button>
-                <button class="btn button-more">
-                    Learn more
-                </button>
-                <a href="#zweite_section"><span></span>Scroll</a>
-                    </div>
-        <div class="col kleiner">
-            2
+<section class="erste_section">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<header>
+					<nav class="navbar navbar-expand-sm">
+						<img src="<?php bloginfo('template_url'); ?>/img/logo_white.svg" class="logo">
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav mr-auto">
+								<li class="nav-item"> <a class="nav-link" href="#"> Home </a>
+								</li>
+								<li class="nav-item"> <a class="nav-link" href="#"> Blog </a>
+								</li>
+								<li class="nav-item"> <a class="nav-link" href="#"> Info </a>
+								</li>
+                            </ul>
+                            P:(800)997-9714 &nbsp;
+							<a href="#">
+								<button class="btn button-start">START A PROJECT</button>
+							</a>
+					</nav>
+				</header>
+				<p class="description">A marketing and brand</p>
+                <p class="description11">creation agency.</p>
+					<p class="description_one">We don't just make logos, we make design & content</p>
+					<p class="description_one">choices that disrupt the market from the inside out.</p>
+					<button class="btn button-started">Get started</button>
+					<button class="btn button-more">Learn more</button>
+					<p class="zentrieren">
+						<a href="#zweite_section"> <i class="arrow down"></i>
+					</p>
+					</a>
+				</p>
+				</div>
         </div>
     </div>
-</div>
 
 
 </section>
 
 <section class="zweite_section" id="zweite_section">
-<div class="container">
-    <div class="row">
-        <div class="col kleiner">
-            1
-        </div>
-        <div class="col-10">
-                <div class="überschrift zentrieren">
-                    <p>OUR EXPERTISE</p>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="überschrift zentrieren">
+					<p>OUR EXPERTISE</p>
+				</div>
+				<div class="description1 zentrieren">We create amazing digital experience and help our customers reach
+					<br>their goals 3x faster than our competitors.</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm rand zentrieren anzeigen">
+					<img src="<?php bloginfo('template_url'); ?>/img/Website_logo.png" class="logo1 hiding_img">
+					<p>Websites</p>
+					<p>We create websites for</p>
+                    <p>humans and humans create websites for us.</p>
+                    <p><input type="button" class="button_examples btn btn-light placeholder" value="Read More"/>
+				</div>
+				<div class="col-sm rand zentrieren anzeigen">
+					<img src="<?php bloginfo('template_url'); ?>/img/Mobile_logo.png" class="logo1 hiding_img">
+					<p>Mobile</p>
+					<p>Enjoy a new mobile</p>
+					<p>experience with respositive</p>
+                    <p>support.</p>
+                    <p><input type="button" class="button_examples btn btn-light placeholder" value="Read More"/>
+				</div>
+				<div class="col-sm rand zentrieren anzeigen">
+                    <img src="<?php bloginfo('template_url'); ?>/img/Mobile_logo.png" class="logo1 hiding_img">
+					<p>Mobile</p>
+					<p>Enjoy a new mobile</p>
+					<p>experience with respositive</p>
+                    <p>support.</p>
+                    <p><input type="button" class="button_examples btn btn-light placeholder" value="Read More"/>
                 </div>
-                <div class="description1 zentrieren">
-                    We create amazing digital experience and help our customers reach <br>
-                    their goals 3x faster than our competitors.
-                </div> 
-        </div> 
-        <div class="col kleiner">
-            2
+				<div class="col-sm rand zentrieren anzeigen">
+					<img src="<?php bloginfo('template_url'); ?>/img/directionsign_logo.png" class="logo1 hiding_img">
+					<p>Design & UX</p>
+					<p>We adjust each detail, so it fits</p>
+                    <p>your expectations.</p>
+                    <p><input type="button" class="button_examples btn btn-light placeholder" value="Read More"/>
+				</div>
+			</div>
+		</div>
+	</div>
+    </div>
+</section>
+
+<section class="dritte_section">
+<p class="text-center">PORTFOLIO</p>
+<p class="text-center">Our philosophy and expertise are best represented by our work </p>
+    <div class="container-fluid oben">
+        <div class="row">
+                <div class="col-4 abstand">
+                    <div class="container abstand">
+                        <img src="<?php bloginfo('template_url'); ?>/img/22.jpg" class="logo2">
+                        <div class="text-block1">
+                        <h4> Digital menu </h4>
+                        <p> Mobile
+                        </p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-4 abstand">
+                    <div class="container abstand">        
+                        <img src="<?php bloginfo('template_url'); ?>/img/55.png" class="logo2">
+                        <div class="text-block1">
+                        <h4> Can you change the world on your </h4>
+                        <p>
+                        </p>
+                    </div>
+                    </div>
+                </div>            
+                <div class="col-4 abstand">
+                    <div class="container abstand">
+                        <img src="<?php bloginfo('template_url'); ?>/img/44.png" class="logo2">
+                        <div class="text-block1">
+                        <h4> Can you change the world on your </h4>
+                        <p>
+                        </p>
+                    </div>
+                    </div>
+                </div>    
         </div>
     </div>
-
-
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-                <div class="col-sm rand zentrieren">
-                    <img src="<?php bloginfo('template_url'); ?>/img/Website_logo.png" class="logo1">
-                    <p>Websites</p>
-                    <p>We create websites for</p>
-                    <p>humans and humans create
-                        websites for us. </p>
+            <div class="col-4 abstand">
+                <div class="container abstand">
+                    <img src="<?php bloginfo('template_url'); ?>/img/33.jpg" class="logo2">
+                    <div class="text-block1">
+                        <h4> Can you change the world on your </h4>
+                        <p>
+                        </p>
+                    </div>
                 </div>
-                <div class="col-sm rand zentrieren">
-                    <img src="<?php bloginfo('template_url'); ?>/img/Mobile_logo.png" class="logo1">
-                    <p>Mobile</p>
-                    <p>Enjoy a new mobile </p>
-                    <p>experience with respositive</p>
-                    <p>support. </p>
+            </div>
+            <div class="col-4 abstand">
+                <div class="container abstand">        
+                    <img src="<?php bloginfo('template_url'); ?>/img/11.jpg" class="logo2">
+                    <div class="text-block1">
+                        <h4> Can you change the world on your </h4>
+                        <p>
+                        </p>
+                    </div>
                 </div>
-                <div class="col-sm rand zentrieren">
-                
-                </div>
-                <div class="col-sm rand zentrieren">
-                    <img src="<?php bloginfo('template_url'); ?>/img/directionsign_logo.png" class="logo1">
-                    <p>Design & UX</p>
-                    <p>We adjust each detail, so it fits</p>
-                    <p>your expectations.</p>
+            </div>            
+            <div class="col-4 abstand">
+                <div class="container abstand">
+                    <img src="<?php bloginfo('template_url'); ?>/img/66.png" class="logo2">
+                    <div class="text-block1">
+                        <h4> Can you change the world on your </h4>
+                        <p>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <div class="zentrieren">
+        <button class="btn  button-load">Load more</button>
+    </div>
 </section>
 
-<!-- <section class="dritte_section zentrieren">
-<p> Portfolio </p>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-4 col-md-4 col-xs-4 thumb">
-            <img src="<?php bloginfo('template_url'); ?>/img/22.jpg" class="logo2">
-        </div>
-        <div class="col-lg-4 col-md-4 col-xs-4 thumb">
-            <img src="<?php bloginfo('template_url'); ?>/img/55.png" class="logo2">
-        </div>
-        <div class="col-lg-4 col-md-4 col-xs-4 thumb">
-            <img src="<?php bloginfo('template_url'); ?>/img/44.png" class="logo2">
-        </div>
-    </div>
-</div>
-<div class="container-fluid">
-    <div class="row überlappen">
-        <div class="col-lg-4 col-md-4 col-xs-4 thumb">
-            <img src="<?php bloginfo('template_url'); ?>/img/33.jpg" class="logo2">
-        </div>
-        <div class="col-lg-4 col-md-4 col-xs-4 thumb">
-            <img src="<?php bloginfo('template_url'); ?>/img/11.jpg" class="logo2">
-        </div>
-        <div class="col-lg-4 col-md-4 col-xs-4 thumb">
-            <img src="<?php bloginfo('template_url'); ?>/img/66.png" class="logo2">
-        </div>
-    </div>
-</div>
-</section> -->
-
-<section>
-    <p class="blog_absatz zentrieren">
+<section class="vierte_section">
+    <p class="blog_absatz text-center">
         BLOG
+    </p>
+    <p class="description22 text-center">
+        Some great marketing insights for you
     </p>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-4">
-                <img src="<?php bloginfo('template_url'); ?>/img/01.png" class="logo3">
+            <div class="col-4 abstand">
+                <div class="container abstand">
+                    <img src="<?php bloginfo('template_url'); ?>/img/01.png" class="logo3">
+                    <div class="text-block">
+                        <h4> Can you change the world on your </h4>
+                        <p>
+                            <img src="<?php bloginfo('template_url'); ?>/img/clock.png" class="logo4">
+                            2 April, 2014
+                            <img src="<?php bloginfo('template_url'); ?>/img/comment.svg" class="logo4">
+                            25
+                        </p>
+                    </div>
+                </div>
             </div>
-            <div class="col-4">
-                <img src="<?php bloginfo('template_url'); ?>/img/02.png" class="logo3">
+            <div class="col-4 abstand">
+                <div class="container abstand">
+                    <img src="<?php bloginfo('template_url'); ?>/img/02.png" class="logo3">
+                    <div class="text-block">
+                        <h4> Can you change the world on your </h4>
+                        <p>
+                            <img src="<?php bloginfo('template_url'); ?>/img/clock.png" class="logo4">
+                            2 April, 2014
+                            <img src="<?php bloginfo('template_url'); ?>/img/comment.svg" class="logo4">
+                            25
+                        </p>
+                    </div>
+                </div>
+                <p>
+                  
+                </p>
             </div>
-            <div class="col-4">
-                <img src="<?php bloginfo('template_url'); ?>/img/03.jpg" class="logo3">
+            <div class="col-4 abstand">
+                <div class="container abstand">
+                    <img src="<?php bloginfo('template_url'); ?>/img/03.jpg" class="logo3">
+                    <div class="text-block">
+                        <h4> Can you change the world on your </h4>
+                        <p>
+                            <img src="<?php bloginfo('template_url'); ?>/img/clock.png" class="logo4">
+                            2 April, 2014
+                            <img src="<?php bloginfo('template_url'); ?>/img/comment.svg" class="logo4">
+                            25
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="zentrieren">
+        <button class="btn button-load1">Load more</button>
+    </div>
+</section>
+
+<section class="fünfte_section">
+    <div class="container">
+        <div class="col-12 text-center">
+            <h1>TESTMONIALS</h1>
+            <p> "Can say about these guys only good works. It has been a pleasure</p>
+            <p> working with them creating the strategy for my small car company</p>
+            <p class="font-weight-bold"> and even some more nice things"</p>
+            <img src="<?php bloginfo('template_url'); ?>/img/72.png" class="logo5 rounded-circle">
+            <p>Agnese Nada, Luxury Cars</p>
+        </div>
+    </div>
+</section>
+
+<section class="sechste_section">
+    <div class="container">
+        <div class="row">
+                <div class="col-12">
+                    <div class="überschrift zentrieren">
+                        <p>MEET THE TEAM</p>
+                    </div>
+                    <div class="description1 zentrieren">Our team values more than big budgets.</p>
+                    <p>We do our best to meet our customers expectations, always in time</p>
+                    </div>
+
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm zentrieren">
+                        <img src="<?php bloginfo('template_url'); ?>/img/154-2.png" class="logo6 rounded-circle">
+                        <p>Maria Brown</p>
+                        <p>Founder & Creative Director</p>
+                    </div>
+                    <div class="col-sm zentrieren">
+                        <img src="<?php bloginfo('template_url'); ?>/img/154-3.png" class="logo6 rounded-circle">
+                        <p>Lia Karolmozz</p>
+                        <p>Development Officer</p>
+                    </div>
+                    <div class="col-sm zentrieren">
+                        <img src="<?php bloginfo('template_url'); ?>/img/154-4.png" class="logo6 rounded-circle">
+                        <p>Amanda Ross</p>
+                        <p>Design & Front-End</p>
+                    </div>
+                    <div class="col-sm zentrieren">
+                        <img src="<?php bloginfo('template_url'); ?>/img/154-1.png" class="logo6 rounded-circle">
+                        <p>John Green</p>
+                        <p>Founder & Creative Director</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 <title><?php wp_title('');?> <?php bloginfo( 'name' ); ?></title>  
-    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">    
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">    
 <?php include "hilfsfunktionen.php" ?>
 <?php wp_head(); ?>
