aa143d993c26c7121ccd47d90dc3038baf415439
Tim Seeger <tim.seeger@stackfuel.com>
2020-02-17 15:41:05 +0100

    Überarbeitet

--- footer.php
+++ footer.php
@@ -1,29 +1,40 @@
 <footer>
     <div class="container">
-        <div class="col-3">    
-            <img src="<?php bloginfo('template_url'); ?>/img/logo_white.svg" class="logo">
-            <p>P:(800)997-9774</p>
-            <p>INFO@STACKFUEL.COM</p>
-            <div class="form-group">
-                <select class="form-control reduzieren" id="auswahl">
-                    <option>ENGLISH</option>
-                    <option>DEUTSCH</option>
-                </select>
+        <div class="row">
+            <div class="col-3">    
+                <img src="<?php bloginfo('template_url'); ?>/img/logo_white.svg" class="logo">
+                <p>P:(800)997-9774</p>
+                <p>INFO@STACKFUEL.COM</p>
+                <div class="form-group">
+                    <select class="form-control reduzieren" id="auswahl">
+                        <option>ENGLISH</option>
+                        <option>DEUTSCH</option>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div class="col-3">
-            Text 
-        </div>
-        <div class="col-3">
-            Text
-        </div>
-        <div class="col-3">
-            Text
-        </div>
-        <p class="bla"></p> 
-        <div class="d-flex icon">
+            <div class="col-3">
+                <p>Explore</p>
+                <p>HOME</p>
+                <p>PORTFOLIO</p>
+                <p>CONTACT</p>
+            </div>
+            <div class="col-3">
+                <p>CONNECT</p>
+                <p>Twitter</P>
+                <p>Facebook</p>
+                <p>Google+</p>
+                <p>LinkedIn</p>
+            </div>
+            <div class="col-3">
+                <p>NEWSLETTER SUBSCRIBE
+                <input class="form-control" placeholder="Email"></p>
 
-        </div>   
+            </div>
+            <p class="bla"></p> 
+            <div class="d-flex icon">
+
+            </div>   
+        </div>
     </div>
     <?php wp_footer(); ?>
 </footer>
