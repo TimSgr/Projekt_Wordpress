61b5a44a991ee402562d073cb43c266f6b6b2bc8
Tim Seeger <tim.seeger@stackfuel.com>
2020-02-21 11:58:10 +0100

    Angepasst

--- page-blog.php
+++ page-blog.php
@@ -1,102 +1,111 @@
 <?php get_header(); ?>
-<section class="section_blog_1">
-	<div class="container">
-		<div class="row">
-			<div class="col-12">
-				<header>
-					<nav class="navbar navbar-expand-sm">
-						<img src="<?php bloginfo('template_url'); ?>/img/logo_white.svg" class="logo">
-						<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<ul class="navbar-nav mr-auto">
-								<li class="nav-item"> <a class="nav-link" href="#"> Home </a>
-								</li>
-								<li class="nav-item"> <a class="nav-link" href="#"> Blog </a>
-								</li>
-								<li class="nav-item"> <a class="nav-link" href="#"> Info </a>
-								</li>
-                            </ul>
-                            P:(800)997-9714 &nbsp;
-							<a href="#">
-								<button class="btn button-start">START A PROJECT</button>
-                            </a>
-                        </div>
-					</nav>
-                </header>
-                <div class="col-md-5">
-                    <p class="description">Secrets & Ideas</p>
-                    <p class="description12">This is how we see the world.</p>
+    <section class="section_blog_1">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <header>
+                        <nav class="navbar navbar-expand-sm">
+                            <img src="<?php bloginfo('template_url'); ?>/img/logo_white.svg" class="logo">
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav mr-auto">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#"> Home </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#"> Blog </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#"> Info </a>
+                                    </li>
+                                </ul>
+                                P:(800)997-9714 &nbsp;
+                                <a href="#">
+                                    <button class="btn button-start">START A PROJECT</button>
+                                </a>
+                            </div>
+                        </nav>
+                    </header>
+                    <div class="col-md-5">
+                        <p class="description">Secrets & Ideas</p>
+                        <p class="description12">This is how we see the world.</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<section class="section_blog_2">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 ">
-                <p class="überschrift1">Can you change the world on your own?</p>
-                <p class="autor_information">
-                    <img src="<?php bloginfo('template_url'); ?>/img/154-1.png" class="logo7 rounded-circle">
-                    by John Brown  
-                    <img src="<?php bloginfo('template_url'); ?>/img/clock.png" class="logo4">
-                    2 April, 2014
-                    <img src="<?php bloginfo('template_url'); ?>/img/comment.svg" class="logo4">
-                    25
-                </p>
-                <div class="col-md-12 my_div">
-                    <!-- Hier sieht man ein Bild -->
-                </div>
-                <div class="beitrag">
-                    <p>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
-                        no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+    </section>
+    <section class="section_blog_2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-6 order-1" id="second">
+                    <p class="überschrift1">Can you change the world on your own?</p>
+                    <p class="autor_information">
+                        <img src="<?php bloginfo('template_url'); ?>/img/154-1.png" class="logo7 rounded-circle"> by John Brown
+                        <img src="<?php bloginfo('template_url'); ?>/img/clock.png" class="logo4"> 2 April, 2014
+                        <img src="<?php bloginfo('template_url'); ?>/img/comment.svg" class="logo4"> 25
                     </p>
-                        <a class="read_more" href="#"><p class="continue"> Continue reading <span class="green"> > </span></a>
+                    <div class="col-md-12 my_div">
+                        <!-- Hier sieht man ein Bild -->
+                    </div>
+                    <div class="beitrag">
+                        <p>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                        </p>
+                        <a class="read_more" href="#">
+                            <p class="continue"> Continue reading <span class="green"> > </span></a>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-4 ">
-                <div class="menu_blog col-md-6">
-                    <p class=" menu_11">&nbsp; Categories</p>
-                    <p class=" menu_11">&nbsp; News</p>
-                    <p class=" menu_11">&nbsp; Strategy</p>
-                    <p class=" menu_11">&nbsp; Brand</p>
-                    <p class=" menu_11">&nbsp; Site</p>
-                    <p class=" menu_11">&nbsp; Internal</p>
+                <div class="col-md-4 col-sm-6 order-2" id="first">
+                    <nav class="site-nav">
+                        <div class="menu">
+                            <ul>
+                                <li class="page_item page-item-102">&nbsp;<span> 📁 Categories</p></span>
+
+                                <li class="page_item page-item-104">&nbsp;
+                                    <span> News ⑳</span>
+                                </li>
+                                <li class="page_item page-item-106">&nbsp;
+                                    <span> Strategy ⑳</span>
+                                </li>
+                                <li class="page_item page-item-108">&nbsp;
+                                    <span> Brand ⑳</span>
+                                </li>
+                                <li class="page_item page-item-110">&nbsp;
+                                    <span> Site ⑳</span>
+                                </li>
+                                <li class="page_item page-item-112">&nbsp;
+                                    <span> Internal ⑳</span>
+                                </li>
+                        
+                        </div>
                 </div>
-            </div>
-                <div class="col-md-8 beitrag">
+                <div class="col-md-8 beitrag order-3" id="third">
                     <p class="überschrift1">Dropbox made some changes</p>
                     <p class="autor_information">
-                        <img src="<?php bloginfo('template_url'); ?>/img/154-3.png" class="logo7 rounded-circle">
-                        by John Brown  
-                        <img src="<?php bloginfo('template_url'); ?>/img/clock.png" class="logo4">
-                        2 April, 2014
-                        <img src="<?php bloginfo('template_url'); ?>/img/comment.svg" class="logo4">
-                        25
+                        <img src="<?php bloginfo('template_url'); ?>/img/154-3.png" class="logo7 rounded-circle"> by John Brown
+                        <img src="<?php bloginfo('template_url'); ?>/img/clock.png" class="logo4"> 2 April, 2014
+                        <img src="<?php bloginfo('template_url'); ?>/img/comment.svg" class="logo4"> 25
                     </p>
                     <p>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
-                        no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
                     </p>
-                        <a class="read_more" href="#"><p class="continue"> Continue reading <span class="green"> > </span></a>
+                    <a class="read_more" href="#">
+                        <p class="continue"> Continue reading <span class="green"> > </span></a>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<section class="section_blog_3">
-    <div class="container">
-        <div class="row">
-            <div class="col-12 justify-content-center text-center">
-                <button class="btn" >❮ Prev</button>
-                <button class="btn">1</button> 
-                <button class="btn">2</button> 
-                <button class="btn">3</button> 
-                <button class="btn">Next ❯</button>
+        
+    </section>
+    <section class="section_blog_3">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 justify-content-center text-center my_setting">
+                    <button class="btn">❮ Prev</button>
+                    <button class="btn">1</button>
+                    <button class="btn">2</button>
+                    <button class="btn">3</button>
+                    <button class="btn">Next ❯</button>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<?php get_footer(); ?>
+    </section>
+    <?php get_footer(); ?>
