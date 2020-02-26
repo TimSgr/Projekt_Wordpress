e29a4f0997d1804689a94dd557758fc7061506c8
Tim Seeger <tim.seeger@stackfuel.com>
2020-02-26 10:10:36 +0100

    Angepasst

--- template_parts/content-team.php
+++ template_parts/content-team.php
@@ -1,4 +1,3 @@
-
-        <?php the_post_thumbnail('full', array('class' => 'logo6 rounded-circle')); ?>
-                <h3> <?php the_title()?> </h3>
-                    <?php the_content(); ?>
+<?php the_post_thumbnail('full', array('class' => 'logo6 rounded-circle')); ?>
+<h3><?php the_title()?></h3>
+<?php the_content(); ?>
