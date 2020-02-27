29d7defd04449023084c67dcc8a31618f7f198bc
Tim Seeger <tim.seeger@stackfuel.com>
2020-02-27 13:40:54 +0100

    Angepasst

--- template_parts/content-testmonials.php
+++ template_parts/content-testmonials.php
@@ -1,11 +1,7 @@
-<p class="oben">
-    <?php the_content(); ?>
-</p>
-<?php the_post_thumbnail('full', array('class' => 'logo5 rounded-circle'))?>
-    <p><?php the_title();?></p>
-
-    <?php
-    previous_post_link('<span class="left black">&laquo; %link</span>');
-    next_post_link('<span class="right black">%link &raquo;</span>');
-?>                           
-<div class="clearfix"></div>
+<li>
+    <p class="oben">
+        <?php the_content(); ?>
+    </p>
+    <?php the_post_thumbnail('full', array('class' => 'logo5 rounded-circle'))?>
+        <p><?php the_title();?></p>                        
+</li>
