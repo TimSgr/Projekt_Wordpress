57c54f676237bd53138a5a0c51d780099ee28a47
Tim Seeger <tim.seeger@stackfuel.com>
2020-03-19 14:56:02 +0100

    Angepasst

--- blog-overview/page-blog.php
+++ blog-overview/page-blog.php
@@ -3,32 +3,26 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <header>
-                        <nav class="navbar navbar-expand-sm">
-                            <img src="<?php bloginfo('template_url'); ?>/img/logo_white.svg" class="logo">
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav mr-auto">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="<?php echo get_home_url(); ?>">
-                                            Home 
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="<?php echo get_permalink( get_option( 'page_for_posts' ) );?>">
-                                            Blog 
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#"> Info </a>
-                                    </li>
-                                </ul>
-                                P:(800)997-9714 &nbsp;
-                                <a href="#">
-                                    <button class="btn button-start">START A PROJECT</button>
-                                </a>
-                            </div>
-                        </nav>
-                    </header>
+				<header>
+					<nav class="navbar navbar-expand-sm">
+						<img src="<?php bloginfo('template_url'); ?>/img/logo_white.svg" class="logo">
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav mr-auto">
+								<li class="nav-item"> <a class="nav-link" href="<?php echo get_home_url(); ?>"> Home </a>
+								</li>
+								<li class="nav-item"> 
+                                    <a class="nav-link" href="<?php echo get_permalink( get_option( 'page_for_posts' ) );?>"> 
+                                        Blog 
+                                    </a>
+								</li>
+                            </ul>
+                            P:(800) 997-9714 &nbsp;
+							<a href="#">
+								<button class="btn button-start">START A PROJECT</button>
+                            </a>
+                        </div>
+					</nav>
+				</header>
                     <div class="col-md-5">
                         <p class="description">Secrets & Ideas</p>
                         <p class="description12">This is how we see the world.</p>
